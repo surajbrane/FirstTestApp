52bf8f511c1335ce23e7af80a4b07aaa02db9114
Suraj Rane <Suraj.Rane@ey.com>
2020-06-29 20:22:26 +0530

    Add project files.

---
+++ FirstTestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
